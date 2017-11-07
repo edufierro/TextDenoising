b0fa0c42958af5c151e52002753ba210dc689ab9
EDUARDO_FIERRO <eff254@nyu.edu>
2017-11-06 20:43:40 -0500

    GloVe runner

---
+++ GloVe/run-glove.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+#SBATCH --job-name=charrrr
+#SBATCH --gres=gpu:1
+#SBATCH --time=23:00:00
+#SBATCH --mem=40GB
+#SBATCH --output=outputs/%A.out
+#SBATCH --error=outputs/%A.err
+
+module purge
+module load python3/intel/3.5.3
+python3 -m pip install http://download.pytorch.org/whl/cu80/torch-0.2.0.post3-cp35-cp35m-manylinux1_x86_64.whl --user --upgrade
+python3 -m pip install torchvision --user --upgrade
+
+cd /scratch/eff254/Optimization/Data/
+
+python3 -u GloVe.py --embedding_dim 200 --top_k 10000 --minibatch 10000 $1 --experiment $2 > logs/edu1.log
