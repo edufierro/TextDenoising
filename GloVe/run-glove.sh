44a1aee0b442698c75c57b2923d3acdd38ad2745
edufierro <edufierro@github.com>
2017-11-06 23:23:00 -0500

    Temp

--- GloVe/run-glove.sh
+++ GloVe/run-glove.sh
@@ -11,5 +11,5 @@ module purge
 module load python3/intel/3.5.3
 module load nltk/python3.5/3.2.4
 
-python3 -m nltk.downloader all
+## python3 -m nltk.downloader all ## Just once. Now I have the folder in my home dir.
 python3 -u GloVe.py --embedding_dim 300 --top_k 10000 --minibatch 10000  --main_data_dir "/scratch/eff254/Optimization/Data/"
