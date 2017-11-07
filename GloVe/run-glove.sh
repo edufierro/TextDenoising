b7fd91d2338b1970ec0cdf540b44b376e618b221
edufierro <edufierro@github.com>
2017-11-06 21:59:48 -0500

    run-glove.sh update

--- GloVe/run-glove.sh
+++ GloVe/run-glove.sh
@@ -12,6 +12,4 @@ module load python3/intel/3.5.3
 python3 -m pip install http://download.pytorch.org/whl/cu80/torch-0.2.0.post3-cp35-cp35m-manylinux1_x86_64.whl --user --upgrade
 python3 -m pip install torchvision --user --upgrade
 
-cd /scratch/eff254/Optimization/Data/
-
-python3 -u GloVe.py --embedding_dim 200 --top_k 10000 --minibatch 10000 $1 --experiment $2 > logs/edu1.log
+python3 -u GloVe.py --embedding_dim 200 --top_k 10000 --minibatch 10000  --main_data_dir "/scratch/eff254/Optimization/Data/TXTsOriginal/"
